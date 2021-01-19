57e61ce2bfd7c9caaef83b0ed249e5ca7ee86738
Sujeevan Rajagopalan <rsujee@MA307.local>
2021-01-19 16:08:04 +0000

    MVP

--- interface.js
+++ interface.js
@@ -1,2 +1,17 @@
-document.getElementById('note').textContent = note.text;
+function updateText(note) {
+  document.getElementById('note').textContent = note.text;
+}
   // document.getElementById('note').innerHTML = note.text
+// function newNote () {
+//   var myNote = new Note();
+//   return myNote
+// }
+  document.getElementById('new-note').onclick = function() {
+    console.log('hello')
+    // event.preventDefault();
+    var myNote = new Note();
+    var text = document.getElementById('text').value;
+    myNote.addText(text);
+    console.log(myNote)
+    updateText(myNote);
+  } 
