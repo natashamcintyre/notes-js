9019e0649f6a2e6e898de5468f0e5650b64a7bf4
ollienorman <oliver.norman1991@gmail.com>
2021-01-20 17:02:10 +0000

    merged and modal added for notes

--- interface.js
+++ interface.js
@@ -1,7 +1,9 @@
 var notes = new Notes();
+
 function updateText(note) {
   document.getElementById('note').textContent = note.text;
 }
+
 function updateList(notes) {
   document.getElementById('note-list').innerHTML = ""
   notes.all().forEach(function(note, index) {
@@ -13,21 +15,17 @@ function updateList(notes) {
     document.getElementById('note-list').appendChild(listItem)
   })
 }
-  // document.getElementById('note').innerHTML = note.text
-// function newNote () {
-//   var myNote = new Note();
-//   return myNote
-// }
-  document.getElementById('new-note').onclick = function() {
-    console.log('hello')
-    // event.preventDefault();
-    var myNote = new Note();
-    var text = document.getElementById('text').value;
-    myNote.addText(text);
-    console.log(myNote)
-    notes.add(myNote)
-    updateList(notes)
-  }
+
+document.getElementById('new-note').onclick = function() {
+  console.log('hello')
+  // event.preventDefault();
+  var myNote = new Note();
+  var text = document.getElementById('text').value;
+  myNote.addText(text);
+  console.log(myNote)
+  notes.add(myNote)
+  updateList(notes)
+}
 
 window.addEventListener("hashchange", function() {
   let id = window.location.hash.split("#")[1];
@@ -36,6 +34,18 @@ window.addEventListener("hashchange", function() {
 })
 
 function display(note) {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "block";
   console.log(`I am in the display(note) function`)
-  document.getElementById('displayed-note').innerHTML = note.text
+  document.getElementById('modal-text').innerHTML = note.text
+  document.getElementsByClassName("close")[0].onclick = function() {
+    modal.style.display = "none";
+    history.replaceState(null, null, ' ');
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      history.replaceState(null, null, ' ');
+    }
+  }
 }
